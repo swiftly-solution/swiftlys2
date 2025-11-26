c377c8023356e5d918bc10d2e3e131b86fd8252e
Ambr0se <eldment@qq.com>
2025-11-26 10:47:24 +0800

    chore: Standardize code to follow conventions

--- managed/src/SwiftlyS2.Shared/Modules/Commands/ICommandContext.cs
+++ managed/src/SwiftlyS2.Shared/Modules/Commands/ICommandContext.cs
@@ -2,18 +2,36 @@ using SwiftlyS2.Shared.Players;
 
 namespace SwiftlyS2.Shared.Commands;
 
-public interface ICommandContext {
+public interface ICommandContext
+{
+    /// <summary>
+    /// Gets a value indicating whether the command was sent by a player.
+    /// </summary>
+    public bool IsSentByPlayer { get; }
 
-  public bool IsSentByPlayer { get; }
+    /// <summary>
+    /// Gets the player who sent the command, or null if the command was not sent by a player.
+    /// </summary>
+    public IPlayer? Sender { get; }
 
-  public IPlayer? Sender { get; }
+    /// <summary>
+    /// Gets the command name itself.
+    /// </summary>
+    public string Prefix { get; }
 
-  public string Prefix { get; }
+    /// <summary>
+    /// Gets a value indicating whether the command should be executed silently without broadcasting to other players.
+    /// </summary>
+    public bool IsSlient { get; }
 
-  public bool IsSlient { get; }
+    /// <summary>
+    /// Gets the array of arguments passed with the command.
+    /// </summary>
+    public string[] Args { get; }
 
-  public string[] Args { get; }
-
-  public void Reply(string message);
-  
+    /// <summary>
+    /// Sends a reply message to the command sender.
+    /// </summary>
+    /// <param name="message">The message to send as a reply.</param>
+    public void Reply( string message );
 }
