d967579a408e5ff6f86c3043c832ca735713c110
Chen Buyi <chenbuyi2019@outlook.com>
2025-12-17 14:22:15 +0800

    Remove unused usings. Format BasePlugin.cs

--- managed/src/SwiftlyS2.Shared/Plugins/BasePlugin.cs
+++ managed/src/SwiftlyS2.Shared/Plugins/BasePlugin.cs
@@ -1,7 +1,3 @@
-using System.Diagnostics;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using SwiftlyS2.Core.Plugins;
 using SwiftlyS2.Shared.Misc;
 
 namespace SwiftlyS2.Shared.Plugins;
@@ -9,31 +5,31 @@ namespace SwiftlyS2.Shared.Plugins;
 public abstract class BasePlugin : IPlugin
 {
 
-  protected ISwiftlyCore Core { get; private init; }
+    protected ISwiftlyCore Core { get; private init; }
 
-  public BasePlugin( ISwiftlyCore core )
-  {
-    Core = core;
+    public BasePlugin( ISwiftlyCore core )
+    {
+        Core = core;
 
-    Console.SetOut(new ConsoleRedirector());
-    Console.SetError(new ConsoleRedirector());
-  }
+        Console.SetOut(new ConsoleRedirector());
+        Console.SetError(new ConsoleRedirector());
+    }
 
-  public virtual void ConfigureSharedInterface( IInterfaceManager interfaceManager ) { }
+    public virtual void ConfigureSharedInterface( IInterfaceManager interfaceManager ) { }
 
-  public virtual void UseSharedInterface( IInterfaceManager interfaceManager ) { }
+    public virtual void UseSharedInterface( IInterfaceManager interfaceManager ) { }
 
-  public virtual void OnSharedInterfaceInjected( IInterfaceManager interfaceManager ) { }
+    public virtual void OnSharedInterfaceInjected( IInterfaceManager interfaceManager ) { }
 
-  public virtual void OnAllPluginsLoaded() { }
+    public virtual void OnAllPluginsLoaded() { }
 
-  public abstract void Load( bool hotReload );
+    public abstract void Load( bool hotReload );
 
-  public abstract void Unload();
+    public abstract void Unload();
 
-  /// <summary>
-  /// You can choose when the plugin is allowed to reload.
-  /// </summary>
-  public virtual PluginReloadMethod ReloadMethod { get; set; } = PluginReloadMethod.Auto;
+    /// <summary>
+    /// You can choose when the plugin is allowed to reload.
+    /// </summary>
+    public virtual PluginReloadMethod ReloadMethod { get; set; } = PluginReloadMethod.Auto;
 
 }
--- managed/src/SwiftlyS2.Shared/Plugins/PluginReloadMethod.cs
+++ managed/src/SwiftlyS2.Shared/Plugins/PluginReloadMethod.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace SwiftlyS2.Shared.Plugins;
 
 public enum PluginReloadMethod
