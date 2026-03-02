63648cecd7f71a87882b7514e4ad3fc5a148be6f
aga <rubin.samuel20@yahoo.ro>
2026-03-02 21:27:43 +0200

    Merge defaults into plugin JSON/TOML configs
    When initializing plugin configs, write default section values only when missing and merge them into existing JSON or TOML files.

--- managed/src/SwiftlyS2.Core/Services/PluginConfigurationService.cs
+++ managed/src/SwiftlyS2.Core/Services/PluginConfigurationService.cs
@@ -1,10 +1,12 @@
 using System.Data.Common;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using Microsoft.Extensions.Configuration;
 using SwiftlyS2.Core.Natives;
 using SwiftlyS2.Core.Services;
 using SwiftlyS2.Shared.Services;
 using Tomlyn;
+using Tomlyn.Model;
 
 namespace SwiftlyS2.Core.Services;
 
@@ -71,25 +73,13 @@ internal class PluginConfigurationService : IPluginConfigurationService
 
   public IPluginConfigurationService InitializeJsonWithModel<T>( string name, string sectionName ) where T : class, new()
   {
-
     var configPath = GetConfigPath(name);
-
-    if (File.Exists(configPath))
-    {
-      return this;
-    }
 
     var dir = Path.GetDirectoryName(configPath);
     if (dir is not null)
     {
       Directory.CreateDirectory(dir);
     }
-
-    var config = new T();
-
-    var wrapped = new Dictionary<string, object?> {
-      [sectionName] = config
-    };
 
     var options = new JsonSerializerOptions {
       WriteIndented = true,
@@ -97,21 +87,66 @@ internal class PluginConfigurationService : IPluginConfigurationService
       PropertyNamingPolicy = null
     };
 
-    var configJson = JsonSerializer.Serialize(wrapped, options);
-    File.WriteAllText(configPath, configJson);
+    var defaults = new Dictionary<string, object?> { [sectionName] = new T() };
+    var defaultJson = JsonSerializer.SerializeToNode(defaults, options)!.AsObject();
+
+    if (!File.Exists(configPath))
+    {
+      File.WriteAllText(configPath, defaultJson.ToJsonString(options));
+      return this;
+    }
+
+    var existingText = File.ReadAllText(configPath);
+    JsonNode? existingNode;
+    try
+    {
+      existingNode = JsonNode.Parse(existingText);
+    }
+    catch (JsonException)
+    {
+      existingNode = null;
+    }
+
+    if (existingNode is not JsonObject existingObj)
+    {
+      File.WriteAllText(configPath, defaultJson.ToJsonString(options));
+      return this;
+    }
+
+    if (MergeJsonObjects(existingObj, defaultJson))
+    {
+      File.WriteAllText(configPath, existingObj.ToJsonString(options));
+    }
 
     return this;
   }
 
+  private static bool MergeJsonObjects( JsonObject target, JsonObject defaults )
+  {
+    var changed = false;
+
+    foreach (var (key, defaultValue) in defaults)
+    {
+      if (!target.ContainsKey(key))
+      {
+        target[key] = defaultValue?.DeepClone();
+        changed = true;
+      }
+      else if (defaultValue is JsonObject defaultChild && target[key] is JsonObject targetChild)
+      {
+        if (MergeJsonObjects(targetChild, defaultChild))
+        {
+          changed = true;
+        }
+      }
+    }
+
+    return changed;
+  }
+
   public IPluginConfigurationService InitializeTomlWithModel<T>( string name, string sectionName ) where T : class, new()
   {
-
     var configPath = GetConfigPath(name);
-
-    if (File.Exists(configPath))
-    {
-      return this;
-    }
 
     var dir = Path.GetDirectoryName(configPath);
     if (dir is not null)
@@ -119,21 +154,68 @@ internal class PluginConfigurationService : IPluginConfigurationService
       Directory.CreateDirectory(dir);
     }
 
-    var config = new T();
-
-    var wrapped = new Dictionary<string, object?> {
-      [sectionName] = config
-    };
-
     var tomlModelOptions = new TomlModelOptions {
-      ConvertPropertyName = name => name,
+      ConvertPropertyName = n => n,
       IgnoreMissingProperties = true
     };
 
-    var tomlString = Toml.FromModel(wrapped, tomlModelOptions);
-    File.WriteAllText(configPath, tomlString);
+    var defaults = new Dictionary<string, object?> { [sectionName] = new T() };
+    var defaultToml = Toml.FromModel(defaults, tomlModelOptions);
+
+    if (!File.Exists(configPath))
+    {
+      File.WriteAllText(configPath, defaultToml);
+      return this;
+    }
+
+    var existingText = File.ReadAllText(configPath);
+    TomlTable? existingTable;
+    try
+    {
+      existingTable = Toml.ToModel(existingText);
+    }
+    catch
+    {
+      existingTable = null;
+    }
+
+    var defaultTable = Toml.ToModel(defaultToml);
+
+    if (existingTable is null)
+    {
+      File.WriteAllText(configPath, defaultToml);
+      return this;
+    }
+
+    if (MergeTomlTables(existingTable, defaultTable))
+    {
+      File.WriteAllText(configPath, Toml.FromModel(existingTable, tomlModelOptions));
+    }
 
     return this;
+  }
+
+  private static bool MergeTomlTables( TomlTable target, TomlTable defaults )
+  {
+    var changed = false;
+
+    foreach (var (key, defaultValue) in defaults)
+    {
+      if (!target.ContainsKey(key))
+      {
+        target[key] = defaultValue;
+        changed = true;
+      }
+      else if (defaultValue is TomlTable defaultChild && target[key] is TomlTable targetChild)
+      {
+        if (MergeTomlTables(targetChild, defaultChild))
+        {
+          changed = true;
+        }
+      }
+    }
+
+    return changed;
   }
 
   public IPluginConfigurationService Configure( Action<IConfigurationBuilder> configure )
