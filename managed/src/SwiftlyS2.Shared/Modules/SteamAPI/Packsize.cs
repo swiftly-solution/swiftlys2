a01224aaea3502a99d9a4c2f91346d8186988547
skuzzis <andreieurt@gmail.com>
2026-09-26 21:26:42 +0300

    update(steamworks): Use correct Pack value

--- managed/src/SwiftlyS2.Shared/Modules/SteamAPI/Packsize.cs
+++ managed/src/SwiftlyS2.Shared/Modules/SteamAPI/Packsize.cs
@@ -4,7 +4,11 @@ namespace SwiftlyS2.Shared.SteamAPI;
 
 public static class Packsize
 {
+    #if WINDOWS
+    public const int value = 8;
+    #else
     public const int value = 4;
+    #endif
 
     [StructLayout(LayoutKind.Sequential, Pack = value)]
     public struct ValvePackingSentinel_t
